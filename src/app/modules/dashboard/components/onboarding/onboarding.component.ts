4d27a36ddb0b406c276881921e90f18e5e314859
Vivek Thakkar <thakkarv29@gmail.com>
2020-09-26 00:24:29 +0530

    veretical stepper added

--- src/app/modules/dashboard/components/onboarding/onboarding.component.ts
+++ src/app/modules/dashboard/components/onboarding/onboarding.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-onboarding',
@@ -7,14 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnboardingComponent implements OnInit {
 
+  // material stepper element ref
+  @ViewChild('stepper') private myStepper: MatStepper;
+
   isLinear: boolean = false;
   currentStep = 0;
 
-  stepsCompletion = [false, false, false, false, false];
+  stepsCompletion = [false, false, false, false];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    
+  }
+
+  ngAfterViewInit(){
+
+    this.previousStepCompleted(2)
+    this.currentStep = 2;
+
+    //subscribe step change events
+    this.myStepper.selectionChange.subscribe(event => {
+      console.log(event, 'emitted...');
+      this.stepsCompletion[event.previouslySelectedIndex] =  true
+    });
+  }
+
+  previousStepCompleted(index) {
+    // makes the previous steps completed
+    while (this.myStepper.selectedIndex < index) {
+      this.stepsCompletion[this.myStepper.selectedIndex] = true;
+      this.myStepper.selectedIndex += 1;
+    }
   }
 
 }
