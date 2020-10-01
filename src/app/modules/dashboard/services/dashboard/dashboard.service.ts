ce7dfe1218cd591089c2bbbecb7f5ee1ad360994
Vivek Thakkar <thakkarv29@gmail.com>
2020-10-01 09:33:13 +0530

    after login apis added in service. sidenav list added for admin

--- src/app/modules/dashboard/services/dashboard/dashboard.service.ts
+++ src/app/modules/dashboard/services/dashboard/dashboard.service.ts
@@ -11,4 +11,25 @@ export class DashboardService {
   getAccountType() {
     return this.http.get(`${environment.backendUrl}/account_type`);
   }
+
+  getServices() {
+    return this.http.get(`${environment.backendUrl}/services`);
+  }
+
+  getFreeTrail() {
+    return this.http.get(`${environment.backendUrl}/free_trail`);
+  }
+
+  getCallFilter() {
+    return this.http.get(`${environment.backendUrl}/callfilter`);
+  }
+
+  getStores() {
+    return this.http.get(`${environment.backendUrl}/stores`);
+  }
+
+  getCallsBySource() {
+    return this.http.get(`${environment.backendUrl}/calls_by_source`);
+  }
+
 }
