2dd25e2c0facc236a803fbfc59293789c51e1047
Vivek Thakkar <thakkarv29@gmail.com>
2020-10-04 20:41:49 +0530

    navbar fixed

--- src/app/modules/dashboard/components/navbar/navbar.component.ts
+++ src/app/modules/dashboard/components/navbar/navbar.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit, OnDestroy, ChangeDetectorRef, ViewChild } from '@angular/core';
-import {MediaMatcher} from '@angular/cdk/layout';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ChangeDetectorRef,
+  ViewChild,
+} from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { Router } from '@angular/router';
 import { NotificationService } from 'src/app/services/notification/notification.service';
@@ -7,51 +13,73 @@ import { NotificationService } from 'src/app/services/notification/notification.
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
   @ViewChild('snav') snav;
 
   mobileQuery: MediaQueryList;
 
-  fillerNav = Array.from({length: 15}, (_, i) => `Nav Item ${i + 1}`);
+  fillerNav = Array.from({ length: 15 }, (_, i) => `Nav Item ${i + 1}`);
 
-  fillerContent = Array.from({length: 50}, () =>
+  fillerContent = Array.from(
+    { length: 50 },
+    () =>
       `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+  );
 
   private _mobileQueryListener: () => void;
 
-  constructor(private authService: AuthService, private router: Router, private notifyService : NotificationService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) { 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private notifyService: NotificationService,
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher
+  ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  ngAfterViewInit(){
-    this.snav.toggle()
+  ngAfterViewInit() {
+    this.snav.toggle();
   }
 
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
-  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some((h) =>
+    h.test(window.location.host)
+  );
 
   logout() {
-    this.authService
-      .logout()
-      .subscribe(() => {
-        this.notifyService.showSuccess( "Logged out successfully !", "Logout")
-        this.router.navigate(['/login'], { replaceUrl: true })
-      });
+    this.authService.logout().subscribe(() => {
+      this.notifyService.showSuccess('Logged out successfully !', 'Logout');
+      this.router.navigate(['/login'], { replaceUrl: true });
+    });
   }
 
+  detectMob() {
+    const toMatch = [
+      /Android/i,
+      /webOS/i,
+      /iPhone/i,
+      /iPad/i,
+      /iPod/i,
+      /BlackBerry/i,
+      /Windows Phone/i,
+    ];
+
+    return toMatch.some((toMatchItem) => {
+      return navigator.userAgent.match(toMatchItem);
+    });
+  }
 }
